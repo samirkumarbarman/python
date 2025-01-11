c403210d0b7c1cd5a61f292106dff576d0d4707f
Samir kumar barman <91568013+samirkumarbarman@users.noreply.github.com>
2025-01-11 12:18:37 +0000

    save

---
+++ practice/hello.py
@@ -0,0 +1,19 @@
+#This is the first code
+"""sytex
+comments
+variables
+"""
+name = "Niparana"
+age = 24
+husbandName = "Samir"
+
+x = 10
+y = 3
+
+def myfunc():
+    global x
+    x =34
+    print(x)
+myfunc()
+
+print(x)
